87230baa0989e21aefdc67cbd606588158abd03c
manju145 <manjuyadav0514001@gmail.com>
2022-11-05 01:14:50 +0530

    files

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+const navbar=()=>{
+    return  `<div id="logo">
+    <a href='/'> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" alt="" /></a>
+   </div>
+   <div id="search">
+   <input type='text'/> 
+ </div>
+ 
+   <div id="options">
+     <a href="/create.html"><button id='submit_btn'>Create Post</button></a>
+   </div>`
+}
+
+export { navbar };
